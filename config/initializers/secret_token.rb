f8899724f491a8aeec1f86682b2031aaabe24b91
Mike Skalnik <mike.skalnik@gmail.com>
2012-03-20 18:37:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlrug3::Application.config.secret_token = '3dc7fb5a1453a892b22a25dd7e11aa38b5fab9f7c8c496995d098acbe79fe4b51120090969a6aa0b584492ea91e4e632e431e7a9e2559d8446027f6c583017cd'
