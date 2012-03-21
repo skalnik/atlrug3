b161e62b27ffbfca314dc2a679fccf5fdc9223b6
Mike Skalnik <mike.skalnik@gmail.com>
2012-03-21 19:25:26 -0400

    Add dropdown for logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def signed_in?
+    !current_user.nil?
+  end
+
+  helper_method :signed_in?
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
