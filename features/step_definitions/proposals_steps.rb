26fdd62924de8c15bab062cf5e2a7f99fa735265
Mike Skalnik <mike.skalnik@gmail.com>
2012-03-20 21:19:47 -0400

    Add admin approval scenario

--- features/step_definitions/proposals_steps.rb
+++ features/step_definitions/proposals_steps.rb
@@ -8,3 +8,19 @@ end
 Then /^I should see the talk was submitted successfully$/ do
   page.body.should match /submitted for review/
 end
+
+Given /^I'm logged in as an admin$/ do
+  pending
+end
+
+Given /^a talk has been submitted$/ do
+  pending
+end
+
+When /^I go approve a talk$/ do
+  pending
+end
+
+Then /^I should see it scheduled for the next meetup$/ do
+  pending
+end
