f638a2f6a6e4471d7ce3bb9a7eb2af188670d9d5
Mike Skalnik <mike.skalnik@gmail.com>
2012-03-21 04:15:44 -0400

    Add login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ATLRUG::Application.routes.draw do
 
   match '/auth/:provider/callback', :to => 'sessions#create'
   match "/pages/*id" => 'pages#show', :as => :page, :format => false
+  match '/login' => redirect('/auth/github'), :as => :login
 
   root :to => 'pages#show', :id => 'home'
 end
