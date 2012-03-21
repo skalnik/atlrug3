b9322a5d3d6c45a2cf7b4f5f354c69463fd7e797
Mike Skalnik <mike.skalnik@gmail.com>
2012-03-21 00:00:47 -0400

    Add user creation and finding by auth hashes from Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   validates :uid, :uniqueness => true
+
+  class << self
+    def find_by_hash(auth_hash)
+      find_by_uid(auth_hash['uid'])
+    end
+
+    def create_from_hash(auth_hash)
+      User.create(:name => auth_hash['info']['name'],
+                  :uid  => auth_hash['uid'])
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,28 @@ require 'spec_helper'
 
 describe User do
   let(:uid)       { '12345' }
+  let(:name)      { 'Mike Skalnik' }
+  let(:auth_hash) { { 'uid' => uid, 'info' => { 'name' => name } } }
 
   context "validations" do
     it "is invalid if it has a non-unique uid" do
-      user1 = Factory.create(:user, :uid => uid)
-      user2 = Factory.build(:user, :uid => uid)
+      user1 = create(:user, :uid => uid)
+      user2 = build(:user, :uid => uid)
       user2.should_not be_valid
+    end
+  end
+
+  describe ".find_by_hash" do
+    it "finds a user by uid" do
+      User.should_receive(:find_by_uid).with(uid)
+      User.find_by_hash(auth_hash)
+    end
+  end
+
+  describe ".create_from_hash" do
+    it "creates a new user with parameters given" do
+      User.should_receive(:create).with(:name => name, :uid => uid)
+      User.create_from_hash(auth_hash)
     end
   end
 end
