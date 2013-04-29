3625fbe77c83001d0b4c9772b85623f37397a490
Al Snow <jasnow@hotmail.com>
2013-04-29 10:06:56 -0400

    upgraded to rails 3.2.13; fixed should_not warnings

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -7,47 +7,47 @@ describe Talk do
   context "validations" do
     it "validates presence of title" do
       talk.title = ""
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates presence of description" do
       talk.description = ""
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates presence of duration" do
       talk.duration = ""
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates numericality of duration" do
       talk.duration = "NaN"
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates postivity of duration" do
       talk.duration = -1
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates duration is an integer" do
       talk.duration = 1.5
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates presence of email" do
       talk.email = ""
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates format of email" do
       talk.email = "invalid email!"
-      should_not be_valid
+      should be_invalid
     end
 
     it "validates format of email" do
       talk.email = "invalid email!"
-      should_not be_valid
+      should be_invalid
     end
   end
 
